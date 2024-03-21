f91469556350aa3267c89ca32dee3b179976b876
Shogo Hyodo <mmoshg8u@gmail.com>
2024-03-21 17:40:57 +0900

    Add debug script

---
+++ examples/debug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SEG_SIZE=$1
+SEG_FILE_DIR=/tmp/output
+
+rm -rf $SEG_FILE_DIR
+
+BASEDIR=test-vectors RUST_LOG=trace ELF_PATH=test-vectors/hello BLOCK_NO=13284491 SEG_OUTPUT=$SEG_FILE_DIR SEG_SIZE=$SEG_SIZE ARGS="" cargo run --release --example zkmips split
+
+SEG_FILE_NUM=$(ls $SEG_FILE_DIR -1 | wc -l)
+echo "segment num: $SEG_FILE_NUM"
+
+for ((i = 0; i < $SEG_FILE_NUM; ++i)); do
+    echo "segment #$i"
+    BASEDIR=test-vectors RUST_LOG=info BLOCK_NO=13284491 SEG_FILE_DIR="$SEG_FILE_DIR" SEG_FILE_NUM=$i SEG_SIZE=$SEG_SIZE cargo run --release --example zkmips aggregate_proof_all_debug
+done
